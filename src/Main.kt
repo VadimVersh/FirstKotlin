8d057de50aa793af7139531c52e6b93c7f61b74e
unknown <vadimkasledopit@gmail.com>
2021-09-20 21:33:21 +0500

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,19 @@
+var numer1 = 34
+var number2 = 8
+
+fun main(args: Array<String>) {
+  var word: String? = null
+    var isHasCar = true
+    word = "Hello"
+    if(word == null){
+        print("Переменная word равна значение null")
+    if(isHasCar)
+        println("Что-то сработало")}
+    else if(word == "Hello")
+        println("Переменная: $word")
+    else
+    { print("переменна Word  не равна значению null")
+    }
+
+
+}
